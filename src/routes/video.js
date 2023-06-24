c98e4ae8679023acc3a2fedcda520d16091857a6
sooraj888 <soorajsagar888@gmail.com>
2023-06-24 23:47:19 +0530

    added file uploade

--- src/routes/video.js
+++ src/routes/video.js
@@ -1,37 +1,83 @@
 const express = require("express");
 const fs = require("fs");
 const rangeParser = require("range-parser");
+const HomeScreenVideoData = require("../models/home");
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const router = new express.Router();
 
 router.get("/video", (req, res) => {
-  const videoPath = "bigbuck.mp4";
-  const videoSize = fs.statSync(videoPath).size;
-  let range = req.headers.range || "bytes=0-";
+  const videoName = req.query.id;
+  console.log("videoName:   ", videoName);
+  const videoPath = `uploads/${videoName}`;
 
-  const positions = rangeParser(videoSize, range, { combine: true });
+  try {
+    const videoSize = fs.statSync(videoPath).size;
 
-  // get video stats (about 61MB)
+    let range = req.headers.range || "bytes=0-";
 
-  const start = positions[0].start;
-  const end = positions[0].end || fileSize - 1;
-  const chunkSize = end - start + 1;
+    const positions = rangeParser(videoSize, range, { combine: true });
 
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": chunkSize,
-    "Content-Type": "video/mp4",
-  };
+    // get video stats (about 61MB)
 
-  // HTTP Status 206 for Partial Content
-  res.writeHead(206, headers);
+    const start = positions[0].start;
+    const end = positions[0].end || fileSize - 1;
+    const chunkSize = end - start + 1;
 
-  // create video read stream for this particular chunk
-  const videoStream = fs.createReadStream(videoPath, { start, end });
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunkSize,
+      "Content-Type": "video/mp4",
+    };
 
-  // Stream the video chunk to the client
-  videoStream.pipe(res);
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+  } catch (e) {
+    if (e?.errno == -4058) {
+      res.status(404).send("video not found");
+    } else {
+      res.status(404).send("something went wrong while fetching video");
+    }
+  }
+});
+
+router.post("/video", upload.single("video"), async (req, res) => {
+  const file = req.file;
+  const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+  try {
+    const addHomeScreenVideoData = new HomeScreenVideoData({
+      videoTitle: file.originalname,
+      views: 0,
+      likes: 0,
+      dislikes: 0,
+      url: fullUrl + "?id=" + file?.filename,
+    });
+
+    console.log("Full URL:", fullUrl);
+    console.log(req.port);
+    const insertHomeScreenData = await addHomeScreenVideoData.save();
+    res.send("File uploaded!");
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 module.exports = router;
